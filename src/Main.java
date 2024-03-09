e86d2f0bc1f951bfe5769d3c19eabe4585df5c23
Şule Nehir ARAÇ <nehir.2706@gmail.com>
2024-03-09 10:43:37 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n,k,total=1;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Üssü alınacak sayıyı giriniz : ");
+        n = input.nextInt();
+        System.out.println("Üs olacak sayıyı giriniz : ");
+        k = input.nextInt();
+
+        for (int i=1; i<=k ; i++ ){
+            total*=n;
+        }
+        System.out.println(total);
+    }
 }
